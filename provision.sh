8588f0e792d8c80a97802a9ada80faed4e1fdf99
odoublewen <owen@myway.com>
2015-02-12 10:32:12 -0800

    Provisioner user is root, not vagrant

--- provision.sh
+++ provision.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get install -y build-essential git postgresql-9.3 postgresql-server-dev-9.3 ipython
+apt-get update
+apt-get install -y build-essential git postgresql-9.3 postgresql-server-dev-9.3 ipython
 
 echo '# "local" is for Unix domain socket connections only
 local   all             all                                  trust
 # IPv4 local connections:
 host    all             all             0.0.0.0/0            trust
 # IPv6 local connections:
-host    all             all             ::/0                 trust' | sudo tee /etc/postgresql/9.3/main/pg_hba.conf
+host    all             all             ::/0                 trust' > /etc/postgresql/9.3/main/pg_hba.conf
 
-sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.3/main/postgresql.conf
-sudo /etc/init.d/postgresql restart
-sudo su - postgres -c 'createuser -s vagrant'
-createdb olidb
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.3/main/postgresql.conf
 
-sudo pip install -r /vagrant/requirements.txt
+/etc/init.d/postgresql restart
+su - postgres -c 'createuser -s vagrant'
+su - vagrant -c 'createdb olidb'
+
+pip install -r /vagrant/requirements.txt
 
 echo "You've been provisioned"
