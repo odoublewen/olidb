89ad600575ca287c4e8f9ec37c91a3005f8b515b
odoublewen <owen@myway.com>
2015-02-11 22:58:02 -0800

    using flask-script and flask-migrate

--- oli/models.py
+++ oli/models.py
@@ -1,67 +1,60 @@
-#!/usr/bin/env python
-#
-import os
-import sys
 import datetime
-from sqlalchemy import Column, ForeignKey, Integer, String, Float, Text, DateTime, types
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.engine import reflection
-from sqlalchemy.schema import MetaData, Table, DropTable, ForeignKeyConstraint, DropConstraint
-import json
-
-Base = declarative_base()
+from oli import db
 
 
-class Oligo(Base):
+class Oligo(db.Model):
     __tablename__ = 'oligo'
-    id = Column(Integer, primary_key=True)
-    oligoset_id = Column(Integer, ForeignKey('oligo_set.id'), nullable=False)
-    seq = Column(String(255), nullable=False)
-    tubename = Column(String(255), nullable=False)
-    probe = Column(String(255), nullable=False)
-    comments = Column(String(255), nullable=False)
-    orderdate = Column(DateTime, nullable=True)
-    created = Column(DateTime, default=datetime.datetime.now)
+    id = db.Column(db.Integer, primary_key=True)
+    oligoset_id = db.Column(db.Integer, db.ForeignKey('oligo_set.id'), nullable=False)
+    seq = db.Column(db.String(255), nullable=False)
+    tubename = db.Column(db.String(255), nullable=False)
+    probe = db.Column(db.String(255), nullable=False)
+    comments = db.Column(db.String(255), nullable=False)
+    orderdate = db.Column(db.DateTime, nullable=True)
+    created = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    def __repr__(self):
+        return '<Tubename %r>' % (self.tubename)
 
 
-class OligoSet(Base):
+class OligoSet(db.Model):
     __tablename__ = 'oligo_set'
-    id = Column(Integer, primary_key=True)
-    setname = Column(String(255), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    setname = db.Column(db.String(255), nullable=False)
 
 
-def cleanslate():
-    conn = engine.connect()
-
-    # the transaction only applies if the DB supports
-    # transactional DDL, i.e. Postgresql, MS SQL Server
-    trans = conn.begin()
-    inspector = reflection.Inspector.from_engine(engine)
-
-    # gather all data first before dropping anything.
-    # some DBs lock after things have been dropped in
-    # a transaction.
-    metadata = MetaData()
-    tbs = []
-    all_fks = []
-    for table_name in inspector.get_table_names():
-        fks = []
-        for fk in inspector.get_foreign_keys(table_name):
-            if not fk['name']:
-                continue
-            fks.append(
-                ForeignKeyConstraint((),(),name=fk['name'])
-                )
-        t = Table(table_name, metadata, *fks)
-        tbs.append(t)
-        all_fks.extend(fks)
-
-    for fkc in all_fks:
-        conn.execute(DropConstraint(fkc))
-
-    for table in tbs:
-        conn.execute(DropTable(table))
-
-    trans.commit()
-
-    Base.metadata.create_all(engine)
+# def cleanslate():
+#     conn = engine.connect()
+#
+#     # the transaction only applies if the DB supports
+#     # transactional DDL, i.e. Postgresql, MS SQL Server
+#     trans = conn.begin()
+#     inspector = reflection.Inspector.from_engine(engine)
+#
+#     # gather all data first before dropping anything.
+#     # some DBs lock after things have been dropped in
+#     # a transaction.
+#     metadata = MetaData()
+#     tbs = []
+#     all_fks = []
+#     for table_name in inspector.get_table_names():
+#         fks = []
+#         for fk in inspector.get_foreign_keys(table_name):
+#             if not fk['name']:
+#                 continue
+#             fks.append(
+#                 ForeignKeyConstraint((),(),name=fk['name'])
+#                 )
+#         t = Table(table_name, metadata, *fks)
+#         tbs.append(t)
+#         all_fks.extend(fks)
+#
+#     for fkc in all_fks:
+#         conn.execute(DropConstraint(fkc))
+#
+#     for table in tbs:
+#         conn.execute(DropTable(table))
+#
+#     trans.commit()
+#
+#     Base.metadata.create_all(engine)
--- run.py
+++ run.py
@@ -1,4 +1,15 @@
+#!/usr/bin/env python
 from oli import app
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    manager.run()
+
