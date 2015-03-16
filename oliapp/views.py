0462b153874c19dacb5a6c7ba2a454f0a79db4b4
Owen Solberg <owen.solberg@gmail.com>
2015-03-15 19:08:42 -0700

    layout of oligoset browser

--- oliapp/views.py
+++ oliapp/views.py
@@ -28,9 +28,21 @@ def oligoset_detail(oligosetid):
 @app.route('/oligoset', defaults={'page': 1})
 @app.route('/oligoset/page/<int:page>')
 def oligoset_browse(page):
-    if 'term' in request.args:
-        g.term = request.args['term']
-    g.pagination = Oligoset.query.join(Target).paginate(page)
+
+    g.term = request.args.get('term')
+    g.taxonomy = request.args.get('taxonomy')
+
+    query = Oligoset.query.join(Target)
+
+    if g.taxonomy:
+        query = query.filter(Target.taxonomy == g.taxonomy)
+
+    if g.term:
+        for term in g.term.split(' '):
+            term = '%%%s%%' % term
+            query = query.filter(Oligoset.name.ilike(term) | Target.namelong.ilike(term))
+
+    g.pagination = query.paginate(page)
     g.active_page = 'oligoset_browse'
     g.taxa = [q.taxa for q in db.session.query(Target.taxonomy.distinct().label("taxa")).order_by(Target.taxonomy).all()]
     return render_template('oligoset_browse.html')
@@ -46,11 +58,9 @@ def experiment_browse(page):
 
 @app.route('/search')
 def site_search():
-    if 'term' in request.args:
-        g.term = request.args['term']
-    # else:
-    #     g.term = 'actin'
-    g.results = search_oligosets(db.session, g.term)
+    g.siteterm = request.args.get('siteterm')
+    g.results = search_oligosets(db.session, g.siteterm)
+
     g.active_page = 'search'
     return render_template('site_search.html')
 
