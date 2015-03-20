976abd863e6ba901f56c166f64fc6638384cc161
Owen Solberg <owen.solberg@gmail.com>
2015-03-19 18:01:17 -0700

    more

--- scripts/reinitialize_database.sh
+++ scripts/reinitialize_database.sh
@@ -2,5 +2,5 @@
 dropdb olidb
 createdb olidb
 rm /vagrant/migrations/versions/*
-/vagrant/manage.py db migrate -m 'initial commit'
-/vagrant/manage.py db upgrade
+./manage.py db migrate -m 'initial commit'
+./manage.py db upgrade
