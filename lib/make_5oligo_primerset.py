025802b7313fc495fe258ee8cbf1bbec10310b1e
Owen Solberg <owen.solberg@gmail.com>
2015-04-02 23:04:06 -0700

    using pandas df to organize primer3 output - additional stuff

--- lib/make_5oligo_primerset.py
+++ lib/make_5oligo_primerset.py
@@ -10,6 +10,7 @@ from tempfile import TemporaryFile, NamedTemporaryFile
 from subprocess import Popen, PIPE
 from re import match, search
 import pandas as pd
+from collections import defaultdict
 
 def temp_fasta(sequence):
     fastahandle = NamedTemporaryFile(delete=False)
@@ -27,8 +28,8 @@ def run_primer3(primer3_in, config_file):
 
     # print primer3_out
     primers = []
-    explain = []
-    # seqid = ''
+    explain = defaultdict(list)
+    seqid = ''
     for l in primer3_out.splitlines():
 
         seqidpatt = search('SEQUENCE_ID=(.+)', l)
@@ -38,57 +39,94 @@ def run_primer3(primer3_in, config_file):
 
         explainpatt = search('EXPLAIN', l)
         if explainpatt:
-            explain.append(l)
+            explain[seqid].append(l)
             continue
 
         primerpatt = match('PRIMER_(LEFT|RIGHT|INTERNAL|PAIR)_([0-9]+)_?(.*)=(.+)', l)
         if primerpatt:
-            primers.append([seqid, primerpatt.group(2), primerpatt.group(1), primerpatt.group(3), primerpatt.group(4)])
+            pid = primerpatt.group(2)
+            end = primerpatt.group(1)
+            metric = primerpatt.group(3)
+            if metric == '':
+                metric = 'COORDS'
+            value = primerpatt.group(4)
+            primers.append([seqid, pid, end, metric, value])
             continue
 
     try:
-        primers = pd.DataFrame(primers, columns=['seqid', 'num', 'end', 'metric', 'value'])
+        primers = pd.DataFrame(primers, columns=['seqid', 'pid', 'end', 'metric', 'value'])
     except ValueError:
         primers = None
 
     return primers, explain
 
 
-def main():
-    # logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)
-
-    seqid = 'gapdh'
-    seq = 'gcctcaagaccttgggctgggactggctgagcctggcgggaggcggggtccgagtcaccgcctgccgccgcgcccccggtttctataaattgagcccgcagcctcccgcttcgctctctgctcctcctgttcgacagtcagccgcatcttcttttgcgtcgccagccgagccacatcgctcagacaccatggggaaggtgaaggtcggagtcaacggatttggtcgtattgggcgcctggtcaccagggctgcttttaactctggtaaagtggatattgttgccatcaatgaccccttcattgacctcaactacatggtttacatgttccaatatgattccacccatggcaaattccatggcaccgtcaaggctgagaacgggaagcttgtcatcaatggaaatcccatcaccatcttccaggagcgagatccctccaaaatcaagtggggcgatgctggcgctgagtacgtcgtggagtccactggcgtcttcaccaccatggagaaggctggggctcatttgcaggggggagccaaaagggtcatcatctctgccccctctgctgatgcccccatgttcgtcatgggtgtgaaccatgagaagtatgacaacagcctcaagatcatcagcaatgcctcctgcaccaccaactgcttagcacccctggccaaggtcatccatgacaactttggtatcgtggaaggactcatgaccacagtccatgccatcactgccacccagaagactgtggatggcccctccgggaaactgtggcgtgatggccgcggggctctccagaacatcatccctgcctctactggcgctgccaaggctgtgggcaaggtcatccctgagctgaacgggaagctcactggcatggccttccgtgtccccactgccaacgtgtcagtggtggacctgacctgccgtctagaaaaacctgccaaatatgatgacatcaagaaggtggtgaagcaggcgtcggagggccccctcaagggcatcctgggctacactgagcaccaggtggtctcctctgacttcaacagcgacacccactcctccacctttgacgctggggctggcattgccctcaacgaccactttgtcaagctcatttcctggtatgacaacgaatttggctacagcaacagggtggtggacctcatggcccacatggcctccaaggagtaagacccctggaccaccagccccagcaagagcacaagaggaagagagagaccctcactgctggggagtccctgccacactcagtcccccaccacactgaatctcccctcctcacagttgccatgtagaccccttgaagaggggaggggcctagggagccgcaccttgtcatgtaccatcaataaagtaccctgtgctcaaccagttaaaaaaaaaaaaaaaaaaaaa'
+def make_inner(seqid, seq, config_file):
 
     # inner taqman primers
-    config_file = 'primer3_config_inner_relaxed.txt'
     primer3_in = "SEQUENCE_ID=%s\n" % seqid
     primer3_in += "SEQUENCE_TEMPLATE=%s\n" % seq
     primer3_in += "SEQUENCE_INCLUDED_REGION=50,%d\n" % (len(seq)-100)
     primer3_in += "=\n"
 
-    p3df, explain3 = run_primer3(primer3_in, config_file=config_file)
+    innerdf, innerexp = run_primer3(primer3_in, config_file=config_file)
 
-    p3coords = p3df[p3df.metric == ''].pivot(index='num', columns='end', values='value')
-    # p3coords['num'] = p3coords.index.astype(int)
-    # p3coords = p3coords.sort(['num'])
+    if innerdf is None:
+        raise RuntimeError(innerexp)
+
+    innerdf['end'] = 'TAQMAN_' + innerdf['end'].astype(str)
+    return innerdf
+
+
+def make_outer(seqid, seq, config_file, p3coords):
 
     # outer flanking primers
-    config_file = 'primer3_config_outer.txt'
     primer3_in = ''
     for index, row in p3coords.iterrows():
-        leftpos = map(int, row.LEFT.split(','))
-        rightpos = map(int, row.RIGHT.split(','))
-        primer3_in += "SEQUENCE_ID=%s__p3__%s\n" % (seqid, index)
+        leftpos = map(int, row.TAQMAN_LEFT.split(','))
+        rightpos = map(int, row.TAQMAN_RIGHT.split(','))
+        primer3_in += "SEQUENCE_ID=%s_____%s\n" % (seqid, index)
         primer3_in += "SEQUENCE_TEMPLATE=%s\n" % seq
         primer3_in += "SEQUENCE_TARGET=%d,%d\n" % (leftpos[0]+leftpos[1]-12, rightpos[0]-rightpos[1]+12)
         primer3_in += "=\n"
 
-    p2df, explain2 = run_primer3(primer3_in, config_file=config_file)
-    # print p2df
+    outerdf, outerexp = run_primer3(primer3_in, config_file=config_file)
+
+    if outerdf is None:
+        raise RuntimeError(outerexp)
+
+    outerdf['end'] = 'PREAMP_' + outerdf['end'].astype(str)
+    outerdf['pid2'] = outerdf['pid']
+    outerdf['seqid'], outerdf['pid'] = zip(*outerdf['seqid'].str.split('_____').tolist())
+    return outerdf
 
 
+def make_5primer_set():
 
+    # logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)
+
+    seqid = 'gapdh'
+    seq = 'gcctcaagaccttgggctgggactggctgagcctggcgggaggcggggtccgagtcaccgcctgccgccgcgcccccggtttctataaattgagcccgcagcctcccgcttcgctctctgctcctcctgttcgacagtcagccgcatcttcttttgcgtcgccagccgagccacatcgctcagacaccatggggaaggtgaaggtcggagtcaacggatttggtcgtattgggcgcctggtcaccagggctgcttttaactctggtaaagtggatattgttgccatcaatgaccccttcattgacctcaactacatggtttacatgttccaatatgattccacccatggcaaattccatggcaccgtcaaggctgagaacgggaagcttgtcatcaatggaaatcccatcaccatcttccaggagcgagatccctccaaaatcaagtggggcgatgctggcgctgagtacgtcgtggagtccactggcgtcttcaccaccatggagaaggctggggctcatttgcaggggggagccaaaagggtcatcatctctgccccctctgctgatgcccccatgttcgtcatgggtgtgaaccatgagaagtatgacaacagcctcaagatcatcagcaatgcctcctgcaccaccaactgcttagcacccctggccaaggtcatccatgacaactttggtatcgtggaaggactcatgaccacagtccatgccatcactgccacccagaagactgtggatggcccctccgggaaactgtggcgtgatggccgcggggctctccagaacatcatccctgcctctactggcgctgccaaggctgtgggcaaggtcatccctgagctgaacgggaagctcactggcatggccttccgtgtccccactgccaacgtgtcagtggtggacctgacctgccgtctagaaaaacctgccaaatatgatgacatcaagaaggtggtgaagcaggcgtcggagggccccctcaagggcatcctgggctacactgagcaccaggtggtctcctctgacttcaacagcgacacccactcctccacctttgacgctggggctggcattgccctcaacgaccactttgtcaagctcatttcctggtatgacaacgaatttggctacagcaacagggtggtggacctcatggcccacatggcctccaaggagtaagacccctggaccaccagccccagcaagagcacaagaggaagagagagaccctcactgctggggagtccctgccacactcagtcccccaccacactgaatctcccctcctcacagttgccatgtagaccccttgaagaggggaggggcctagggagccgcaccttgtcatgtaccatcaataaagtaccctgtgctcaaccagttaaaaaaaaaaaaaaaaaaaaa'
+
+    seqid = 'ACTB'
+    seq = 'ACCGCCGAGACCGCGTCCGCCCCGCGAGCACAGAGCCTCGCCTTTGCCGATCCGCCGCCCGTCCACACCCGCCGCCAGCTCACCATGGATGATGATATCGCCGCGCTCGTCGTCGACAACGGCTCCGGCATGTGCAAGGCCGGCTTCGCGGGCGACGATGCCCCCCGGGCCGTCTTCCCCTCCATCGTGGGGCGCCCCAGGCACCAGGGCGTGATGGTGGGCATGGGTCAGAAGGATTCCTATGTGGGCGACGAGGCCCAGAGCAAGAGAGGCATCCTCACCCTGAAGTACCCCATCGAGCACGGCATCGTCACCAACTGGGACGACATGGAGAAAATCTGGCACCACACCTTCTACAATGAGCTGCGTGTGGCTCCCGAGGAGCACCCCGTGCTGCTGACCGAGGCCCCCCTGAACCCCAAGGCCAACCGCGAGAAGATGACCCAGATCATGTTTGAGACCTTCAACACCCCAGCCATGTACGTTGCTATCCAGGCTGTGCTATCCCTGTACGCCTCTGGCCGTACCACTGGCATCGTGATGGACTCCGGTGACGGGGTCACCCACACTGTGCCCATCTACGAGGGGTATGCCCTCCCCCATGCCATCCTGCGTCTGGACCTGGCTGGCCGGGACCTGACTGACTACCTCATGAAGATCCTCACCGAGCGCGGCTACAGCTTCACCACCACGGCCGAGCGGGAAATCGTGCGTGACATTAAGGAGAAGCTGTGCTACGTCGCCCTGGACTTCGAGCAAGAGATGGCCACGGCTGCTTCCAGCTCCTCCCTGGAGAAGAGCTACGAGCTGCCTGACGGCCAGGTCATCACCATTGGCAATGAGCGGTTCCGCTGCCCTGAGGCACTCTTCCAGCCTTCCTTCCTGGGCATGGAGTCCTGTGGCATCCACGAAACTACCTTCAACTCCATCATGAAGTGTGACGTGGACATCCGCAAAGACCTGTACGCCAACACAGTGCTGTCTGGCGGCACCACCATGTACCCTGGCATTGCCGACAGGATGCAGAAGGAGATCACTGCCCTGGCACCCAGCACAATGAAGATCAAGATCATTGCTCCTCCTGAGCGCAAGTACTCCGTGTGGATCGGCGGCTCCATCCTGGCCTCGCTGTCCACCTTCCAGCAGATGTGGATCAGCAAGCAGGAGTATGACGAGTCCGGCCCCTCCATCGTCCACCGCAAATGCTTCTAGGCGGACTATGACTTAGTTGCGTTACACCCTTTCTTGACAAAACCTAACTTGCGCAGAAAACAAGATGAGATTGGCATGGCTTTATTTGTTTTTTTTGTTTTGTTTTGGTTTTTTTTTTTTTTTTGGCTTGACTCAGGATTTAAAAACTGGAACGGTGAAGGTGACAGCAGTCGGTTGGAGCGAGCATCCCCCAAAGTTCACAATGTGGCCGAGGACTTTGATTGCACATTGTTGTTTTTTTAATAGTCATTCCAAATATGAGATGCGTTGTTACAGGAAGTCCCTTGCCATCCTAAAAGCCACCCCACTTCTCTCTAAGGAGAATGGCCCAGTCCTCTCCCAAGTCCACACAGGGGAGGTGATAGCATTGCTTTCGTGTAAATTATGTAATGCAAAATTTTTTTAATCTTCGCCTTAATACTTTTTTATTTTGTTTTATTTTGAATGATGAGCCTTCGTGCCCCCCCTTCCCCCTTTTTTGTCCCCCAACTTGAGATGTATGAAGGCTTTTGGTCTCCCTGGGAGTGGGTGGAGGCAGCCAGGGCTTACCTGTACACTGACTTGAGACCAGTTGAATAAAAGTGCACACCTTAAAAATGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+
+
+    try:
+        innerdf = make_inner(seqid, seq, 'primer3_config_inner.txt')
+    except RuntimeError:
+        innerdf = make_inner(seqid, seq, 'primer3_config_inner_relaxed.txt')
+
+    # get the coordinates of each primer pair, so that we can target the flanking primers
+    p3coords = innerdf[innerdf.metric == 'COORDS'].pivot(index='pid', columns='end', values='value')
+    # config_file = 'primer3_config_outer.txt'
+    config_file = 'primer3_config_outer_relaxed.txt'
+    outerdf = make_outer(seqid, seq, config_file, p3coords)
+
+    # print innerdf
+    # print outerdf
+    df = innerdf.append(outerdf)
+    print df
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -107,4 +145,4 @@ if __name__ == '__main__':
     else:
         loglevel = logging.INFO
 
-    main()
+    make_5primer_set()
