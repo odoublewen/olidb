f3a75fc33bb69c668d5ede32ba350c6abd2faa56
Owen Solberg <owen.solberg@roche.com>
2016-09-11 08:41:26 -0700

    rebooting django efforts

--- common/settings.py
+++ common/settings.py
@@ -79,10 +79,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'olidb',
-        'USER': 'vagrant',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
     }
 }
 
--- provision.sh
+++ provision.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y git postgresql-9.3 postgresql-server-dev-9.3 libcurl4-openssl-dev python3-pip rabbitmq-server unzip ncurses-dev
-apt-get install -y emacs24-nox byobu htop primer3 
+apt update
+apt install -y git postgresql-9.5 postgresql-server-dev-9.5 libcurl4-openssl-dev python3-pip rabbitmq-server unzip ncurses-dev bowtie
+apt install -y emacs24-nox byobu htop primer3
 #apt-get install -y build-essential
 
 echo '# "local" is for Unix domain socket connections only
@@ -18,31 +18,9 @@ sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgre
 su - postgres -c 'createuser -s vagrant'
 su - vagrant -c 'createdb olidb'
 
-# cd ~
-# wget -q https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tar.xz
-# tar xf Python-3.5.1.tar.xz
-# cd Python-3.5.1
-# ./configure --prefix=/usr/local
-# make && make altinstall
-# cd /usr/local/bin
-# ln -s python3.5 pip3.5 .
 pip3 install --upgrade pip
 pip3 install Cython
 pip3 install -r /vagrant/requirements.txt
-
-cd ~
-wget -q http://downloads.sourceforge.net/project/bowtie-bio/bowtie/1.1.2/bowtie-1.1.2-linux-x86_64.zip
-unzip bowtie-1.1.2-linux-x86_64.zip
-rm bowtie-1.1.2-linux-x86_64.zip
-mv bowtie-1.1.2 /opt
-
-echo PATH=/opt/bowtie-1.1.2:$PATH >> /etc/profile
-
-wget -q https://dl.dropboxusercontent.com/u/24849204/bowtie2_indexes.tar.gz
-tar xf bowtie2_indexes.tar.gz
-rm bowtie2_indexes.tar.gz
-mv bowtie2_indexes /opt
-
 
 # adding these commands to the bash history just for convenience
 su - vagrant -c "echo './scripts/reinitialize_database.sh
